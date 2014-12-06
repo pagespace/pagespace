99abc1b1dac54955f1a4478d1944e11dd96159a1
Phil Mander <philip.mander@gmail.com>
2014-12-06 11:40:35 +0100

    Acl exits early if permission found

--- src/misc/acl.js
+++ src/misc/acl.js
@@ -27,7 +27,7 @@ Acl.prototype.isAllowed = function(role, resource, action) {
     var allowed = false;
 
     var permission, i;
-    for(i = this.permissions.length - 1; i >= 0; i--) {
+    for(i = this.permissions.length - 1; i >= 0 && !allowed; i--) {
         permission = this.permissions[i];
         if(permission.resourcePattern.test(resource) && permission.actions.indexOf(action) > -1) {
             allowed = permission.roles.indexOf(role) > -1;
