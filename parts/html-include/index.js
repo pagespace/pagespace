7b964a6444fc1bf67968ec69746fb261681a07f3
Phil Mander <philip.mander@gmail.com>
2014-12-30 19:51:55 +0100

    HTML include part

--- parts/html-include/index.js
+++ parts/html-include/index.js
@@ -1,14 +1,34 @@
 'use strict';
+var BluebirdPromise = require('bluebird');
+var path = require('path');
+var fs = require('fs');
+
+var readFileAsync = BluebirdPromise.promisify(fs.readFile)
+
+var cache = {};
 
 module.exports = {
     viewPartial: null,
     init: function(viewPartial) {
         this.viewPartial = viewPartial;
     },
-    process: function(data) {
-        if(!data) {
-            data = '<!-- Web copy -->';
+    process: function(data, support) {
+
+        //read from cache
+        if(data && data.file && cache[data.file]) {
+            return cache[data.file];
         }
-        return data;
+
+        //read from fs
+        if(data && data.file) {
+            var filePath = path.resolve(support.basePath, data.file);
+            var promise = readFileAsync(filePath, 'utf8');
+            promise.then(function(val) {
+                cache[data.file] = val;
+            });
+            return promise;
+        }
+
+        return 'No file specified';
     }
 };
