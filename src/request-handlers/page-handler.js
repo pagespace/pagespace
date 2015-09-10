03a5b389caabe996987afaad761bd191a03a5b1f
Phil Mander <philip.mander@gmail.com>
2015-09-10 16:33:38 +0200

    refactor: tidy up page handler

--- src/request-handlers/page-handler.js
+++ src/request-handlers/page-handler.js
@@ -21,18 +21,26 @@
 
 var util = require('util'),
     url = require('url'),
-
     Promise = require('bluebird'),
-
     psUtil = require('../misc/pagespace-util');
 
-var redirectStatuses = [ 301, 302, 303, 307 ];
-
-var PageHandler = function() {
+var httpStatus = {
+    OK: 200,
+    NOT_FOUND: 404,
+    GONE: 410,
+    SERVER_ERROR: 500,
+    REDIRECTS: [ 301, 302, 303, 307 ]
 };
+
+var PageHandler = function() {};
 
 module.exports = new PageHandler();
 
+/**
+ * Initalizes the page handler middleware
+ * @param support
+ * @returns {Function}
+ */
 PageHandler.prototype.init = function(support) {
 
     this.logger = support.logger;
@@ -51,7 +59,7 @@ PageHandler.prototype.init = function(support) {
 };
 
 /**
- * Process a valid request
+ * Process a page request
  */
 PageHandler.prototype.doRequest = function(req, res, next) {
 
@@ -60,19 +68,6 @@ PageHandler.prototype.doRequest = function(req, res, next) {
 
     var urlPath = url.parse(req.url).pathname;
 
-    function sessionValueSwitch(req, queryParam, sessionKey) {
-        if(req.query[queryParam]) {
-            if(req.user && req.user.role !== 'guest' && psUtil.typeify(req.query[queryParam]) === true) {
-                logger.debug('Switching %s on', sessionKey);
-                req.session[sessionKey] = true;
-            } else if(psUtil.typeify(req.query[queryParam]) === false) {
-                logger.debug('Switching %s off', sessionKey);
-                req.session[sessionKey] = false;
-            }
-        }
-        return req.session[sessionKey] || false;
-    }
-
     var previewMode = sessionValueSwitch(req, '_preview', 'preview');
     logger.info('New %s page request', (previewMode ? 'preview' : 'live'));
 
@@ -80,10 +75,12 @@ PageHandler.prototype.doRequest = function(req, res, next) {
     var Page = this.dbSupport.getModel('Page', modelModifier);
     var pageQueryCachKey = urlPath + '_' + modelModifier;
 
-    ///clear cache if in preview mode
     if(previewMode) {
+        ///clear cache if in preview mode
         this.findPagePromises[pageQueryCachKey] = null;
     }
+
+    //create the page query and execute it and cache it
     if(!this.findPagePromises[pageQueryCachKey]) {
         var filter = {
             url: urlPath
@@ -93,157 +90,42 @@ PageHandler.prototype.doRequest = function(req, res, next) {
         this._setFindPagePromise(pageQueryCachKey, findPage());
     }
 
+    //get the page from the db!
     this.findPagePromises[pageQueryCachKey].then(function(page) {
+        var status = page.status || httpStatus.NOT_FOUND;
 
-        logger.debug('Page retrieved');
+        var pageProps = {
+            page: page,
+            status: status,
+            previewMode: previewMode,
+            urlPath: urlPath
+        };
 
-        page = page || {};
-
-        var err;
-        var promises = {};
-        if(page.status) {
-            //status is good
-            promises.status = page.status;
-        } else {
-            //doesn't exist create 404
-            page.status = 404;
+        if(page.status === httpStatus.OK) {
+            logger.debug('Page found (%s) for %s: %s', status, urlPath, page.id);
+            //each region may need to be processed, this may be async
+            pageProps = self.getProcessedPageRegions(req, logger, page, Page, pageProps);
         }
 
-        if(page.status === 200) {
-            //page found and is ok
-
-            logger.debug('Page found (200) for %s: %s', urlPath, page.id);
-
-            promises.page = page;
-
-            //read data for each plugin
-            page.regions.forEach(function (region, regionIndex) {
-                region.includes.forEach(function(include, includeIndex) {
-                    var pluginModule = self.pluginResolver.require(include.plugin ? include.plugin.module : null);
-                    var includeId = regionIndex + '_' + includeIndex;
-                    if (pluginModule) {
-                        var regionData = include.data || {};
-                        if (typeof pluginModule.process === 'function') {
-                            promises[includeId] = pluginModule.process(regionData, {
-                                basePath: self.userBasePath,
-                                PageModel: Page,
-                                req: req,
-                                logger: logger.child({plugin: include.plugin.name})
-                            });
-                        } else {
-                            promises[includeId] = regionData;
-                        }
-                    }
-                });
-            });
-        } else if(page.status === 404) {
-            //page is a 404
-            logger.info('Request is 404, passing to next()');
-            err = new Error('Page not found (404) for: ' + urlPath);
-            err.status = 404;
-            throw err;
-        } else if(page.status === 410) {
-            //page is a 410 (gone)
-            logger.info('Request is 410, passing to next()');
-            err = new Error('Page gone (410) for: ' + urlPath);
-            err.status = 410;
-            throw err;
-        } else if(redirectStatuses.indexOf(page.status) >= 0) {
-            //page is a redirect (301, 302, 303, 307)
-            logger.info('Request is %s, handling redirect', page.status);
-            promises.redirect = page.redirect;
+        return Promise.props(pageProps);
+    }).then(function(pageResult) {
+        var status = pageResult.status;
+        if(status === httpStatus.OK) {
+            //all include plugins have resolved and the page can be rendered
+            self.doPage(req, res, next, logger, pageResult);
+        } else if(httpStatus.REDIRECTS.indexOf(status) >= 0) {
+            //handle redirects
+            logger.info('Request is %s, handling redirect', status);
+            self.doRedirect(req, res, next, logger, pageResult);
+        } else if(status === httpStatus.NOT_FOUND || status === httpStatus.GONE) {
+            //not found or gone
+            self.doNotFound(logger, pageResult);
         } else {
-            logger.warn('Page with unsupported status requested (%s)', page.status);
-        }
-        return Promise.props(promises);
-    }).then(function(result) {
-        var err;
-        var status = result.status;
-        if(status === 200) {
-            var page = result.page;
-
-            var pageData = {};
-            pageData.site = self.site;
-            pageData.page = page.toObject();
-            pageData.preview = previewMode;
-            pageData.live = !previewMode;
-
-            //template properties
-            pageData.template = {};
-            page.template.properties.forEach(function(prop) {
-                pageData.template[prop.name] = prop.value;
-            });
-
-            page.regions.forEach(function (region, regionIndex) {
-                pageData[region.name] = {
-                    data: []
-                };
-
-                var aggregatedViewPartials = [];
-                region.includes.forEach(function(include, includeIndex) {
-                    var pluginModule = self.pluginResolver.get(include.plugin ? include.plugin.module : null);
-                    var htmlWrapper, viewPartial;
-                    if(pluginModule) {
-                        var includeId = regionIndex + '_' + includeIndex;
-                        pageData[region.name].data[includeIndex] = {
-                            data: result[includeId] || {}
-                        };
-                        viewPartial = pluginModule.__viewPartial ?
-                            pluginModule.__viewPartial : 'The view partial could not be resolved';
-                        if(previewMode) {
-                            htmlWrapper =
-                                '<div data-plugin="%s" ' +
-                                'data-page-id="%s" ' +
-                                'data-region="%s" ' +
-                                'data-include="%s">\n%s\n</div>';
-                            viewPartial = util.format(htmlWrapper, pluginModule.__config.name, page._id, region.name,
-                                includeIndex, viewPartial);
-                        } else {
-                            htmlWrapper = '<div>\n%s\n</div>';
-                            viewPartial = util.format(htmlWrapper, viewPartial);
-                        }
-                    } else {
-                        pageData[region.name].data[includeIndex] = {
-                            data: null
-                        };
-                        htmlWrapper = '<!-- Region: %s, Include %s -->';
-                        viewPartial = util.format(htmlWrapper, region.name, includeIndex);
-                    }
-                    aggregatedViewPartials.push('{{#with data.[' + includeIndex + ']}}' + viewPartial + '{{/with}}');
-                });
-                self.viewEngine.registerPartial(region.name, aggregatedViewPartials.join('\n'), urlPath);
-            });
-
-            var templateSrc = !page.template ? 'default.hbs' : page.template.src;
-
-            //force an extension
-            if(!/\.hbs$/.test(templateSrc)) {
-                templateSrc += '.hbs';
-            }
-            pageData.__template = urlPath;
-            res.render(templateSrc, pageData, function(err, html) {
-                if(err) {
-                    logger.error(err, 'Trying to render page');
-                    next(err);
-                } else {
-                    logger.info('Page request processed OK in %s ms', Date.now() - req.startTime);
-                    res.send(html);
-                }
-            });
-        } else if(redirectStatuses.indexOf(status) >= 0) {
-            //redirects
-            var redirectPage = result.redirect;
-            if(redirectPage && redirectPage.url) {
-                res.redirect(status, redirectPage.url);
-            } else {
-                logger.warn('Page to redirect to is not set. Sending 404');
-                err = new Error('Page not found for ' + urlPath);
-                err.status = 404;
-                throw err;
-            }
-        } else {
-            err = new Error('Status not supported');
-            err.status = status;
+            //something else?
+            var message = util.format('Status %s is not supported for pages', status);
+            logger.warn(message);
+            var err = new Error(message);
+            err.status = 500;
             next(err);
         }
     }).catch(function(err) {
@@ -252,6 +134,12 @@ PageHandler.prototype.doRequest = function(req, res, next) {
     });
 };
 
+/**
+ * Caches the promise for finding a page by its url. Expires after a minute for live mode
+ * @param key
+ * @param promise
+ * @private
+ */
 PageHandler.prototype._setFindPagePromise = function(key, promise) {
     var self = this;
     this.findPagePromises[key] = promise;
@@ -261,3 +149,182 @@ PageHandler.prototype._setFindPagePromise = function(key, promise) {
         delete self.findPagePromises[key];
     }, 60 * 1000);
 };
+
+/**
+ * Returns a map of page includes to their data (which may need to be processed by the plugin)
+ * @param logger
+ * @param page
+ * @param pageModel
+ * @param pageProps
+ * @returns {*}
+ */
+PageHandler.prototype.getProcessedPageRegions = function(req, logger, page, pageModel, pageProps) {
+
+    var self = this;
+
+    //read data for each plugin
+    page.regions.forEach(function (region, regionIndex) {
+        region.includes.forEach(function(include, includeIndex) {
+            var pluginModule = self.pluginResolver.require(include.plugin ? include.plugin.module : null);
+            var includeId = regionIndex + '_' + includeIndex;
+            if (pluginModule) {
+                var regionData = include.data || {};
+                if (typeof pluginModule.process === 'function') {
+                    pageProps[includeId] = pluginModule.process(regionData, {
+                        basePath: self.userBasePath,
+                        PageModel: pageModel,
+                        req: req,
+                        logger: logger.child({plugin: include.plugin.name})
+                    });
+                } else {
+                    pageProps[includeId] = regionData;
+                }
+            }
+        });
+    });
+
+    return pageProps;
+};
+
+/**
+ * Resolves a page.
+ * @param req
+ * @param res
+ * @param next
+ * @param logger
+ * @param pageResult
+ */
+PageHandler.prototype.doPage = function(req, res, next, logger, pageResult) {
+
+    var self = this;
+
+    var page = pageResult.page;
+
+    var pageData = {};
+    pageData.site = self.site;
+    pageData.page = page.toObject();
+    pageData.preview = pageResult.previewMode;
+    pageData.live = !pageResult.previewMode;
+
+    //template properties
+    pageData.template = {};
+    page.template.properties.forEach(function(prop) {
+        pageData.template[prop.name] = prop.value;
+    });
+
+    page.regions.forEach(function (region, regionIndex) {
+        pageData[region.name] = {
+            data: []
+        };
+
+        var aggregatedViewPartials = [];
+        region.includes.forEach(function(include, includeIndex) {
+            var pluginModule = self.pluginResolver.get(include.plugin ? include.plugin.module : null);
+            var htmlWrapper, viewPartial;
+            if(pluginModule) {
+                var includeId = regionIndex + '_' + includeIndex;
+                pageData[region.name].data[includeIndex] = {
+                    data: pageResult[includeId] || {}
+                };
+                viewPartial = pluginModule.__viewPartial ?
+                    pluginModule.__viewPartial : 'The view partial could not be resolved';
+                if(pageResult.previewMode) {
+                    htmlWrapper =
+                        '<div data-plugin="%s" ' +
+                        'data-page-id="%s" ' +
+                        'data-region="%s" ' +
+                        'data-include="%s">\n%s\n</div>';
+                    viewPartial = util.format(htmlWrapper, pluginModule.__config.name, page._id, region.name,
+                        includeIndex, viewPartial);
+                } else {
+                    htmlWrapper = '<div>\n%s\n</div>';
+                    viewPartial = util.format(htmlWrapper, viewPartial);
+                }
+            } else {
+                pageData[region.name].data[includeIndex] = {
+                    data: null
+                };
+                htmlWrapper = '<!-- Region: %s, Include %s -->';
+                viewPartial = util.format(htmlWrapper, region.name, includeIndex);
+            }
+
+            //wrap each include in a with to give it the correct context
+            aggregatedViewPartials.push('{{#with data.[' + includeIndex + ']}}' + viewPartial + '{{/with}}');
+        });
+
+        //each page has its own handlebars instance and partials are cached for that instance usign the url as a key
+        self.viewEngine.registerPartial(region.name, aggregatedViewPartials.join('\n'), pageResult.urlPath);
+    });
+
+    var templateSrc = !page.template ? 'default.hbs' : page.template.src;
+
+    //force an extension
+    if(!/\.hbs$/.test(templateSrc)) {
+        templateSrc += '.hbs';
+    }
+    pageData.__template = pageResult.urlPath;
+    res.render(templateSrc, pageData, function(err, html) {
+        if(err) {
+            logger.error(err, 'Unable to render page');
+            next(err);
+        } else {
+            logger.info('Page request processed OK in %s ms', Date.now() - req.startTime);
+            res.send(html);
+        }
+    });
+};
+
+/**
+ * Sends redirects
+ * @param req
+ * @param res
+ * @param next
+ * @param logger
+ * @param pageResult
+ */
+PageHandler.prototype.doRedirect = function(req, res, next, logger, pageResult) {
+    //redirects
+    var redirectPage = pageResult.redirect;
+    if(redirectPage && redirectPage.url) {
+        res.redirect(pageResult.status, redirectPage.url);
+    } else {
+        logger.warn('Page to redirect to is not set. Sending 404');
+        var err = new Error('Page not found for ' + pageResult.urlPath);
+        err.status = 404;
+        throw err;
+    }
+};
+
+/**
+ * Throws an error for missing pages (404 and 410)
+ * @param logger
+ * @param result
+ * @param urlPath
+ */
+PageHandler.prototype.doNotFound = function(logger, pageResult) {
+    //page is a 404
+    var status = pageResult.status;
+    logger.info('Request is %s, passing to next()', status);
+    var errMessage  = status === httpStatus.GONE ? 'The page, %s, has gone (%s)' : 'Page not found for %s (%s)';
+    var err = new Error(util.format(errMessage, pageResult.urlPath, status));
+    err.status = status;
+    throw err;
+};
+
+/**
+ * Utility to check session values set by query params (such as preview mode)
+ * @param req
+ * @param queryParam
+ * @param sessionKey
+ * @returns {*|boolean}
+ */
+function sessionValueSwitch(req, queryParam, sessionKey) {
+    if(req.query[queryParam]) {
+        if(req.user && req.user.role !== 'guest' && psUtil.typeify(req.query[queryParam]) === true) {
+            req.session[sessionKey] = true;
+        } else if(psUtil.typeify(req.query[queryParam]) === false) {
+            req.session[sessionKey] = false;
+        }
+    }
+    return req.session[sessionKey] || false;
+}
