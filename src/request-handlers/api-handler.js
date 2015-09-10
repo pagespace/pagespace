a65105d4de7cc1868218b32727b376650fe1ccd7
Phil Mander <philip.mander@gmail.com>
2015-09-10 16:32:20 +0200

    refactor: Split into several methods and user mongoose 4.x promises

--- src/request-handlers/api-handler.js
+++ src/request-handlers/api-handler.js
@@ -19,13 +19,39 @@
 
 'use strict';
 
-var consts = require('../app-constants'),
+var util = require('util'),
+    consts = require('../app-constants'),
     psUtil = require('../misc/pagespace-util');
+
+//maps model ur namel partss to model names
+var modelMap = {
+    sites: 'Site',
+    pages: 'Page',
+    plugins: 'Plugin',
+    templates:'Template',
+    users: 'User',
+    media: 'Media'
+};
+
+//fields to auto populate when making queries to these model names (the keys)
+var populationsMap = {
+    Site: '',
+    Page: 'parent template regions.includes.plugin redirect createdBy updatedBy',
+    Plugin: '',
+    Template: 'regions.includes.plugin',
+    User: '',
+    Media: ''
+};
 
 var ApiHandler = function() {};
 
 module.exports = new ApiHandler();
 
+/**
+ * Initialize the API middleware
+ * @param support
+ * @returns {Function}
+ */
 ApiHandler.prototype.init = function(support) {
 
     this.logger = support.logger;
@@ -38,31 +64,17 @@ ApiHandler.prototype.init = function(support) {
     };
 };
 
+/**
+ * Handle API requests
+ * @param req
+ * @param res
+ * @param next
+ */
 ApiHandler.prototype.doRequest = function(req, res, next) {
 
     var logger = psUtil.getRequestLogger(this.logger, req, 'api', ++this.reqCount);
 
     logger.info('New api request');
-
-    //maps collection names to models
-    var modelMap = {
-        sites: 'Site',
-        pages: 'Page',
-        plugins: 'Plugin',
-        templates:'Template',
-        users: 'User',
-        media: 'Media'
-    };
-
-    //fields to auto populate when making queries to these collcetions (the keys)
-    var populationsMap = {
-        sites: '',
-        pages: 'parent template regions.includes.plugin redirect createdBy updatedBy',
-        plugins: '',
-        templates: 'regions.includes.plugin',
-        users: '',
-        media: ''
-    };
 
     var apiInfo = consts.requests.API.regex.exec(req.url);
     var apiType = apiInfo[1];
@@ -77,107 +89,161 @@ ApiHandler.prototype.doRequest = function(req, res, next) {
         delete req.body._id;
         delete req.body.__v;
 
-        var docData;
-        if(req.method === 'GET') {
-            var filter = {};
-            if(itemId) {
-                filter._id = itemId;
-                logger.debug('Searching for items by id [%s]: ' + modelName, itemId);
-            } else {
-                logger.debug('Searching for items in model: ' + modelName);
-            }
+        //map a http method to a function to call
+        var methodMapping = {
+            GET : 'doGet',
+            POST : 'doCreate',
+            PUT: 'doUpdate',
+            DELETE: 'doDelete'
+        };
 
-            //create a filter from the query string
-            for(var p in req.query) {
-                //use __ prefix to stop special query params being included in filter
-                if(req.query.hasOwnProperty(p) && p.indexOf('__') !== 0) {
-                    filter[p] = psUtil.typeify(req.query[p]);
-                }
-            }
-
-            var populations = psUtil.typeify(req.query.__nopop) ? '' : populationsMap[apiType];
-            Model.find(filter, '-__v').populate(populations).sort('-createdAt').exec(function(err, results) {
-                if(err) {
-                    logger.error(err, 'Error trying API GET for %s', apiType);
-                    return next(err);
-                } else {
-                    logger.info('API request OK in %s ms', Date.now() - req.startTime);
-                    results =  itemId ? results[0] : results;
-                    if(req.headers.accept && req.headers.accept.indexOf('application/json') === -1) {
-                        var html = psUtil.htmlStringify(results);
-                        return res.send(html, {
-                            'Content-Type' : 'text/html'
-                        }, 200);
-                    } else {
-                        return res.json(results);
-                    }
-                }
-            });
-        } else if(req.method === 'POST') {
-            if(itemId) {
-                logger.warn('Cannot POST for this url. It shouldn\'t contain an id [%s]', itemId);
-                next();
-            } else {
-                logger.info('Creating new %s', modelName);
-                logger.debug('Creating new model with data: ' );
-                logger.debug(req.body);
-
-                docData = req.body;
-                var model = new Model(docData);
-                model.createdBy = req.user._id;
-                model.save(function(err, model) {
-                    if(err) {
-                        logger.error(err, 'Trying to save for API POST for %s', apiType);
-                        next(err);
-                    } else {
-                        logger.info('API post OK in %s ms', Date.now() - req.startTime);
-                        res.json(model);
-                    }
-                });
-            }
-        } else if(req.method === 'PUT') {
-            if(!itemId) {
-                logger.warn('Cannot PUT for this url. It should contain an id');
-                next();
-            } else {
-                logger.info('Updating %s with id [%s]', modelName, itemId);
-                logger.debug('Updating model with data: ' );
-                docData = req.body;
-                docData.updatedBy = req.user._id;
-                this.draft = true;
-                logger.debug(req.body);
-                Model.findOneAndUpdate({_id: itemId}, docData, { 'new' : true }, function (err, doc) {
-
-                    if (err) {
-                        logger.error(err, 'Trying to update for API PUT for %s', apiType);
-                        next(err);
-                    } else {
-                        logger.info('API PUT OK in %s ms', Date.now() - req.startTime);
-                        res.json(doc);
-                    }
-                });
-            }
-        } else if(req.method === 'DELETE') {
-            if (!itemId) {
-                logger.warn('Cannot DELETE for this url. It should contain an id');
-                next();
-            } else {
-                logger.info('Removing %s with id [%s]', modelName, itemId);
-                Model.findByIdAndRemove(itemId, function (err) {
-                    if (err) {
-                        logger.error(err, 'Trying to do API DELETE for %s', apiType);
-                        next(err);
-                    } else {
-                        logger.info('API DELETE OK in %s ms', Date.now() - req.startTime);
-                        res.statusCode = 204;
-                        res.send();
-                    }
-                });
-            }
+        var fnName = methodMapping[req.method.toUpperCase()];
+        if(fnName) {
+            this[fnName](req,res, next, logger, Model, itemId);
         } else {
             var err = new Error('Unrecognized method');
             err.status = 405;
             next(err);
         }
+    }
+};
+
+/**
+ * Get from the DB
+ * @param req
+ * @param res
+ * @param next
+ * @param logger
+ * @param Model
+ * @param itemId
+ */
+ApiHandler.prototype.doGet = function get(req, res, next, logger, Model, itemId) {
+    var filter = {};
+    if (itemId) {
+        filter._id = itemId;
+        logger.debug('Searching for items by id [%s]: %s', itemId, Model.modelName);
+    } else {
+        logger.debug('Searching for items in model: %s', Model.modelName);
+    }
+
+    //create a filter from the query string
+    for (var p in req.query) {
+        //use __ prefix to stop special query params being included in filter
+        if (req.query.hasOwnProperty(p) && p.indexOf('__') !== 0) {
+            filter[p] = psUtil.typeify(req.query[p]);
+        }
+    }
+
+    var populations = psUtil.typeify(req.query.__nopop) ? '' : populationsMap[Model.modelName];
+    Model.find(filter, '-__v').populate(populations).sort('-createdAt').then(function (results) {
+        logger.info('API request OK in %s ms', Date.now() - req.startTime);
+        results = itemId ? results[0] : results;
+        if (req.headers.accept && req.headers.accept.indexOf('application/json') === -1) {
+            var html = psUtil.htmlStringify(results);
+            res.send(html, {
+                'Content-Type': 'text/html'
+            }, 200);
+        } else {
+            res.json(results);
+        }
+    }).then(undefined, function (err) {
+        logger.error(err, 'Error trying API GET for %s', Model.modelName);
+        next(err);
+    });
+};
+
+/**
+ * Create a doc in the DB
+ * @param req
+ * @param res
+ * @param next
+ * @param logger
+ * @param Model
+ * @param itemId
+ */
+ApiHandler.prototype.doCreate = function create(req, res, next, logger, Model, itemId) {
+    if (itemId) {
+        var message = util.format('Cannot POST for this url. It should not contain an id [%s]', itemId);
+        logger.warn(message);
+        var err = new Error(message);
+        err.status = 400;
+        next(err);
+    } else {
+        logger.info('Creating new %s', Model.modelName);
+        logger.debug('Creating new model with data: ');
+        logger.debug(req.body);
+
+        var docData = req.body;
+        var model = new Model(docData);
+        model.createdBy = req.user._id;
+        model.save().then(function (model) {
+            logger.info('API post OK in %s ms', Date.now() - req.startTime);
+            res.json(model);
+        }).then(undefined, function (err) {
+            logger.error(err, 'Trying to save for API POST for %s', Model.name);
+            next(err);
+        });
+    }
+};
+
+/**
+ * Update a doc in the DB
+ * @param req
+ * @param res
+ * @param next
+ * @param logger
+ * @param Model
+ * @param itemId
+ */
+ApiHandler.prototype.doUpdate = function update(req, res, next, logger, Model, itemId) {
+    if (!itemId) {
+        var message = 'Cannot PUT for this url. It should contain an id';
+        logger.warn(message);
+        var err = new Error(message);
+        err.status = 400;
+        next(err);
+    } else {
+        logger.info('Updating %s with id [%s]', Model.modelName, itemId);
+        logger.debug('Updating model with data: ');
+        var docData = req.body;
+        docData.updatedBy = req.user._id;
+        this.draft = true;
+        logger.debug(req.body);
+        Model.findOneAndUpdate({_id: itemId}, docData, { 'new': true }).then(function (doc) {
+            logger.info('API PUT OK in %s ms', Date.now() - req.startTime);
+            res.json(doc);
+        }).then(undefined, function (err) {
+            logger.error(err, 'Trying to update for API PUT for %s', Model.modelName);
+            next(err);
+        });
+    }
+};
+
+/**
+ * Delete a doc in the DB
+ * @param res
+ * @param req
+ * @param next
+ * @param logger
+ * @param Model
+ * @param itemId
+ */
+ApiHandler.prototype.doDelete = function del(res, req, next, logger, Model, itemId) {
+    if (!itemId) {
+        var message = 'Cannot delete for this url. It should contain an id';
+        logger.warn(message);
+        var err = new Error(message);
+        err.status = 400;
+        next(err);
+    } else {
+        logger.info('Removing %s with id [%s]', Model.modelName, itemId);
+        Model.findByIdAndRemove(itemId).then(function () {
+            logger.info('API DELETE OK in %s ms', Date.now() - req.startTime);
+            res.statusCode = 204;
+            res.send();
+        }).then(undefined, function (err) {
+            logger.error(err, 'Trying to do API DELETE for %s', Model.modelName);
+            next(err);
+        });
     }
 };
