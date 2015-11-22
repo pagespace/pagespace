5c93988680b37b80339437c66230b3e81a98fd25
Phil Mander <philip.mander@gmail.com>
2015-11-23 00:19:54 +0100

    Update travis config to use mongo 3

--- clean.sh
+++ clean.sh
@@ -1,6 +1,5 @@
 echo Cleaning database
 mongo test --eval "db.dropDatabase()"
 mongorestore --db test spec/e2e/fixtures/db/test
-mongo test --eval 'db.createUser({ user: "tester", pwd: "test", roles: [{ role: "readWrite", db: "test" }]})'
 echo Cleaning upload directory
 rm media-uploads/* -f
