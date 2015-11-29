6ab652a2db19f1a973b09286a621e90e22e485ff
Phil Mander <philip.mander@gmail.com>
2015-11-29 14:14:30 +0100

    Fixing travis build config 2

--- clean.sh
+++ clean.sh
@@ -2,5 +2,5 @@ echo Cleaning database
 mongo test --eval "db.dropDatabase()"
 mongorestore --db test spec/e2e/fixtures/db/test
 echo Cleaning upload directory
-rm media-uploads/* -
-mongo test --eval 'db.addUser({ user: "tester", pwd: "test", roles: [{ role: "readWrite", db: "test" }] })'
+rm media-uploads/* -f
+mongo test --eval 'db.addUser({ user: "tester", pwd: "test", roles: [{ role: "admin", db: "test" }] })'
